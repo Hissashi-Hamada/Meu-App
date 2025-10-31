9aaff6102a3deaa0bac1f1631775b507190c72e7
enzo <enzohhamada@gmail.com>
2025-10-31 18:45:52 -0300

    chore: initial commit

---
+++ resources/views/layouts/parts/content-header.blade.php
@@ -0,0 +1,25 @@
+        <div class="app-content-header">
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6">
+                @hasSection ('page-title')
+                    <h3 class="mb-0">@yield('page-title')</h3>  
+                @endif
+
+                @isset($breadcrumbs)
+                    <ol class="breadcrumb">
+                        @foreach ( $breadcrumbs as $breadcrumb )
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                        @endforeach
+                    </ol>                           
+                @endisset
+                
+              </div>
+
+              <div class="col-sm-6">
+
+              </div>
+            </div>
+            <!--end::Row--></div></div>
