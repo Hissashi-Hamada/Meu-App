a477bc8ae62b956bbd978a56849956d0b09530df
enzo <enzohhamada@gmail.com>
2025-11-04 16:38:01 -0300

    aula 56 tudo certo ate aqui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,96 +1,42 @@
 @extends('layouts.auth')
 @section('body-class','login-page')
 @section('content')
-<div class="login-box">
-  <div class="login-logo">
-    <a href="../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="{{ route('login') }}" method="post">
-        @csrf
-
-        {{-- Email --}}
-        <div class="input-group mb-3">
-          <div class="input-group-text">
-            <span class="bi bi-envelope"></span>
-          </div>
-          <input
-            type="email"
-            name="email"
-            class="form-control @error('email') is-invalid @enderror"
-            placeholder="Email"
-            value="{{ old('email') }}"
-          />
-          @error('email')
-            <div class="invalid-feedback">
-              {{ $message }}
-            </div>
-          @enderror
-        </div>
-
-        {{-- Senha --}}
-        <div class="input-group mb-3">
-          <div class="input-group-text">
-            <span class="bi bi-lock-fill"></span>
-          </div>
-          <input
-            type="password"
-            name="password"
-            class="form-control @error('password') is-invalid @enderror"
-            placeholder="Password"
-          />
-          @error('password')
-            <div class="invalid-feedback">
-              {{ $message }}
-            </div>
-          @enderror
-        </div>
-
-        {{-- Lembrar-me + botão --}}
-        <div class="row">
-          <div class="col-8">
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                name="remember"
-                id="remember"
-              />
-              <label class="form-check-label" for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">
-              Login
-            </button>
-          </div>
-        </div>
-      </form>
-
-      <!-- /.social-auth-links -->
-      <div class="d-grid gap-2 text-center">
-        <br />
-        <hr style="border: 0; border-top: 1px solid #484c6b91; margin: 0.9rem 0;" />
-        <p class="mb-1">
-          <a href="{{ route('password.request') }}">I forgot my password</a>
-        </p>
-        <hr style="border: 0; border-top: 1px solid #484c6b91; margin: 0.9rem 0;" />
-        <p class="mb-0">
-          <a href="{{ route('register') }}" class="text-center">
-            Register a new membership
-          </a>
-        </p>
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="../index2.html"><b>Admin</b>LTE</a>
       </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Login in to start your session</p>
+          <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+              <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}"/>
+                @error('email')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+              <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"" placeholder="Password" />
+                @error('password')
+                  <div class="invalid-feedback">
+                    {{ $message }}  
+                  </div>
+                @enderror
+            </div>
+              <div class="d-grid gap-2">
+              <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+            </form>
+            <div class="mt-2 text-center">
+              <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
+              <p class="mb-0"><a href="register.html" class="text-center"> Register a new membership </a></p>
+            </div>
+        </div>
     </div>
-    <!-- /.login-card-body -->
-  </div>
-</div>
   @endsection
